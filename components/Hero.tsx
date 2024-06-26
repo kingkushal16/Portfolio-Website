38dc6d63251217eb1317a48b90e2fad018ffb6ef
Kushal J <jkushal16@gmail.com>
2024-06-26 12:08:06 +0530

    Fixed apos bugs for vercel

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,7 +21,7 @@ export const Hero = () => {
             <div className="flex justify-center relative my-20 z-10">
               <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
                 <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
-                  Welcome to Kushal's Portfolio Website
+                  Welcome to Kushal&apos;s Portfolio Website
                 </h2>
                 
                 <TextGenerateEffect className='text-center text-[40px] md:text-5xl lg:text-6xl' words="Embrace the Will to Change"></TextGenerateEffect>
